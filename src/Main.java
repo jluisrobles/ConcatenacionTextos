eda273d86b0bfd365399cd620b5f0cfcc0a7f4dc
Robles <joseroblesarce@yahoo.es>
2023-02-09 11:07:53 +0000

    Concatenacion de textos

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        String texto1 = "Jose Luis";
+        String texto2 = "Rodriguez";
+        String texto3 = "Zapatero";
+
+        System.out.println(texto1 + " " + texto2 + " " + texto3);
+    }
+}
